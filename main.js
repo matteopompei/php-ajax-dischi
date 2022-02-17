4012b736a55927b01e1afb01e64f0902355edaa3
Matteo Pompei <datxizzan@gmail.com>
2022-02-17 18:06:12 +0100

    Funzionante

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
 var app = new Vue({
   el: "#app",
   data: {
-    albumArr = []
+    albumArr: [],
+    loading: true,
+  },
+  created() {
+    this.getAlbum();
   },
   methods: {
-   getAlbum() {
-    axios.get('http://localhost/api.php')
-    .then((response) => {
-      // handle success
-      console.log(response);
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    })
-   }
-  }
+    getAlbum() {
+      axios
+        .get("http://localhost/api.php")
+        .then((response) => {
+          // handle success
+          console.log(response.data);
+          this.albumArr = response.data;
+          console.log(this.albumArr);
+          this.loading = false;
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        });
+    },
+  },
 });
